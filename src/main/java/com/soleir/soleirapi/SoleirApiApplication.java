0d6a65146b26884e12427b6168738fb176d1c856
Bebhinn Hare <Bebhin@Duncans-iMac.local>
2021-04-21 11:06:58 +0100

    Inital setup

---
+++ src/main/java/com/soleir/soleirapi/SoleirApiApplication.java
@@ -0,0 +1,13 @@
+package com.soleir.soleirapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoleirApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoleirApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soleir/soleirapi/SoleirApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soleir.soleirapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoleirApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
